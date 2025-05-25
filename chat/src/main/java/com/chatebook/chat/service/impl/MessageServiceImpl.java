0f7556c0659abc656298446bb8517fcf3f4be7b5
phduchuy <phanhoangduchuyy@gmail.com>
2025-05-25 20:51:04 +0700

    update: update query ai response

--- chat/src/main/java/com/chatebook/chat/service/impl/MessageServiceImpl.java
+++ chat/src/main/java/com/chatebook/chat/service/impl/MessageServiceImpl.java
@@ -29,7 +29,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 @RequiredArgsConstructor
